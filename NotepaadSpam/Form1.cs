4711375b40d25ae9b23d41eb20e09e051e3e71db
EmmyMalware <159585735+Emmyrain@users.noreply.github.com>
2024-02-10 09:29:12 -0600

    Add files via upload

---
+++ NotepaadSpam/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NotepaadSpam.Properties;
+
+// hey yall its emmy but i forgor how to code c# now so ill try my best
+
+namespace NotepaadSpam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //some code for disabled buttons and exiting
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            MessageBox.Show("ARE YOU REALLY SURE YOU WANT TO RUN THIS?", "LAST WARNING", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Trying to find something?????";
+        }
+    }
+}
---
+++ NotepaadSpam/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+// NotepadSpam by EmmyMalware
+// why are you decompiling the code to see this
+// Code last edited December 3rd, 2023
+
+namespace NotepaadSpam
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
